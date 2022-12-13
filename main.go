de7c347dedd0c4caeac699ccf0fdefc66d113efb
siaoynli <siaoynli@126.com>
2022-12-13 10:58:47 +0800

    配置

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Prefork:       true,
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Fiber",
+		AppName:       "Test App v1.0.1",
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World222!")
